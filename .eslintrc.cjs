dd379a80b38b2e0e61964c23f628f25238e37f43
Sprinterson <charles.raviaud@protonmail.com>
2022-09-22 12:27:15 +0200

    feat: error handler middleware

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,10 @@ module.exports = {
     strict: 0,
     'no-param-reassign': 0,
     'import/prefer-default-export': 0,
+    'import/extensions': [0, 'ignorePackages'],
+  },
+  settings: {
+    version: 'detect',
   },
   env: {
     jest: true,
--- server.js
+++ server.js
@@ -1,19 +1,31 @@
-/* eslint-disable import/extensions */
+/* eslint-disable no-console */
 import express from 'express';
 import dotenv from 'dotenv';
+import 'express-async-errors';
+import morgan from 'morgan';
 
 import connectDB from './db/connect.js';
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
 import notFoundMiddleWare from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import authenticateUser from './middleware/auth.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome!');
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+
+app.get('/api/v1', (req, res) => {
+  res.json({ msg: 'Welcome' });
 });
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
 
